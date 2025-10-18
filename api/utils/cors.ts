21e4c26b571b2bffa4e3fad8d7267502693a794c
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-10-18 09:45:04 +0800

    fix: typos

--- api/utils/cors.ts
+++ api/utils/cors.ts
@@ -6,21 +6,21 @@ const ALLOWED_ORIGINS = [
   'https://pxwg.github.io'
 ];
 
-export const handleCors = (handler: (req: VercelRequest, res: VercelResponse) => Promise<void>) => 
-  async (req: VercelRequest, res: VercelResponse) => {
-    const origin = req.headers.origin;
-    if (origin && ALLOWED_ORIGINS.includes(origin)) {
-      res.setHeader('Access-Control-Allow-Origin', origin);
-    }
-    
+export function handleCors(req: VercelRequest, res: VercelResponse): boolean {
+  const origin = req.headers.origin || '';
+
+  if (origin && ALLOWED_ORIGINS.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-    
-    if (req.method === 'OPTIONS') {
-      res.status(200).end();
-      return;
-    }
-    
-    return await handler(req, res);
-};
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+
+  }
+
+  if (req.method === 'OPTIONS') {
+    res.status(204).end();
+    return true;
+  }
+
+  return false;
+}
