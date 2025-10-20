40c8b14f1d93855e0a836364b984fe5f1528ca41
pxwg <xiangxy23@mails.tsinghua.edu.cn>
2025-10-20 15:48:32 +0800

    fix: add white list

--- api/utils/cors.ts
+++ api/utils/cors.ts
@@ -1,10 +1,11 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 const ALLOWED_ORIGINS = [
-  'http://localhost:4321', 
+  'http://localhost:4321',
   'https://localhost:4321',
   'https://homeward-sky.top',
-  'https://pxwg.github.io'
+  'https://api.homeward-sky.top',
+  'https://pxwg.github.io',
 ];
 
 export function handleCors(req: VercelRequest, res: VercelResponse): boolean {
@@ -14,8 +15,10 @@ export function handleCors(req: VercelRequest, res: VercelResponse): boolean {
     res.setHeader('Access-Control-Allow-Origin', origin);
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, cache-control, pragma');
-
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization, cache-control, pragma'
+    );
   }
 
   if (req.method === 'OPTIONS') {
